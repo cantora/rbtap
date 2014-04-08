58f80701a99479fa662b088a7689bc7ff9605845
anthony cantor <ar.cantor@gmail.com>
2014-04-07 18:15:43 -0600

    licensed code to gplv3

--- lib/rbtap/DB.rb
+++ lib/rbtap/DB.rb
@@ -1,3 +1,18 @@
+#Copyright 2014 anthony cantor
+#This file is part of rbtap.
+#
+#rbtap is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#rbtap is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with rbtap.  If not, see <http://www.gnu.org/licenses/>.
 require 'singleton'
 require 'active_record'
 require 'digest/sha1'
